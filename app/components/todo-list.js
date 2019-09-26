c4e8cdf10a1532a4882bf88928dfbcfcc9c3c2a4
Martin <martind@hopon.co.il>
2019-09-26 17:36:58 +0300

    Move to component and start using computed variables to calculate remaining tasks

---
+++ app/components/todo-list.js
@@ -0,0 +1,33 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+
+
+export default Component.extend({
+  store: service(),
+  todos: null,
+  actions: {
+    createOne(title) {
+      if (!title.trim()) { return; }
+
+      var todo = this.get('store').createRecord('task', {
+        name: title,
+        completed: false
+      });
+
+      this.set('newTitle', '');
+      todo.save();
+    },
+    toggleCompleted(task) {
+      task.set('completed', !task.get('completed'));
+      task.save();
+    },
+    delete(task) {
+      task.destroyRecord();
+    }
+  },
+  remaining: computed('todos.@each.completed', function() {
+    let todos = this.get('todos');
+    return todos.filterBy('completed', false).length;
+  })
+});
